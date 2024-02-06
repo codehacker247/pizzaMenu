80d3d8641f00496cf7a0e188319c086ee6cabcde
Rohit Kumar <rohitkumarmandal34@gmail.com>
2024-02-06 18:19:37 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -50,7 +50,6 @@ const pizzaData = [
 function App() {
   return (
     <div className="container">
-      <h1>Hellow World!</h1>
       <Header />
       <Menu />
       <Footer />
@@ -70,15 +69,29 @@ function Header() {
 }
 
 function Menu() {
+  const pizzas = pizzaData;
+  //const pizzas = [];
+  const numPizzas = pizzas.length;
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <div>
-        {" "}
-        {pizzaData.map((pizza) => (
-          <Pizza pizzaObj={pizza} />
-        ))}
-      </div>
+
+      {numPizzas > 0 ? (
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzaData.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
+      ) : (
+        <p>We're still working on the menu. Please come back later :)</p>
+      )}
+
       {/* <Pizza
         name="Pizza Spinnaci"
         ingredients="Tomata, maozarella, spinach, and ricotta cheese"
@@ -96,16 +109,18 @@ function Menu() {
   );
 }
 
-const Pizza = (props) => {
+const Pizza = ({ pizzaObj }) => {
+  // if (pizzaObj.soldOut) return null;
+
   return (
-    <div className="pizza">
-      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
-        <h3>{props.pizzaObj.name}</h3>
-        <p>{props.pizzaObj.ingredients}</p>
-        <span>{props.pizzaObj.price + 3}</span>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
       </div>
-    </div>
+    </li>
   );
 };
 
@@ -120,12 +135,33 @@ function Footer() {
   // } else {
   //   alert("Sorry we're Closed");
   // }
+
+  // if (!isOpen) return <p>CLOSED</p>;// but it then renders everything and footer component will not include
+
   return (
     <footer className="footer">
-      {new Date().toLocaleDateString()}. We are crrently Open.
+      {isOpen ? (
+        <Order closeHour={closeHour} openHour={openHour} />
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+        </p>
+      )}
     </footer>
   );
   //return React.createElement("footer", null, "We're currently open!");
+}
+
+function Order({ closeHour, openHour }) {
+  return (
+    <div className="order">
+      <p>
+        We're open from {openHour}:00 to {closeHour}:00. Come visit us or order
+        online.
+      </p>
+      <button className="btn">Order</button>
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
